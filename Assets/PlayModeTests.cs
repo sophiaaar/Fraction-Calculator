38be43e9e108d4a0d7703e956d08d6260647b883
Sophia <sophiaclarke93@gmail.com>
2017-05-25 18:53:54 +0100

    I wrote a unit test?!
    Still figuring this out. Wrote a unit test to check the Add method in
    the Fraction class

---
+++ Assets/PlayModeTests.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class PlayModeTests {
+	public Fraction _Fraction;
+
+	[Test]
+	public void PlayModeTestsSimplePasses() {
+		// Use the Assert class to test conditions.
+	}
+
+	// [Test]
+	// public void GameObject_CreatedWithGiven_WillHaveTheName()
+	// {
+	// 	var go = new GameObject(“MyGameObject”);
+	// 	Assert.AreEqual(“MyGameObject”, go.name);
+	// }
+
+	[Test]
+	public void TestAddition()
+	{
+		Fraction testFractionOne = new Fraction(1, 2);
+		Fraction testFractionTwo = new Fraction(1, 4);
+
+		Fraction expectedResults = new Fraction (3, 4);
+
+		Fraction actualResults = _Fraction.Add(testFractionOne, testFractionTwo);
+		Assert.AreSame(expectedResults, actualResults);
+
+	}
+
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator PlayModeTestsWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+}
